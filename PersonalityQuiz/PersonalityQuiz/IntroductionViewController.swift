c71eca91f48df7d96dcfcd1ff997c2b47757628b
Kim Hyorim <hyorm1226@gmail.com>
2021-06-20 14:06:23 +0900

    many things

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by HyoRim Kim on 2021/03/13.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
